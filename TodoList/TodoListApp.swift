61f3e3894d3722034366783be35ed710fac1bfe3
ndsarno <ndsarno16@gmail.com>
2021-03-02 15:43:42 -0600

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Nick Sarno on 3/2/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
